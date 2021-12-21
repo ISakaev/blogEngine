3714031806cf56fed465c78f6d7d84351539684d
isakaev <isakaev@akber-soft.ru>
2021-12-21 17:31:36 +0500

    Stage 1, correct data and application.yml

--- src/main/java/ru/isakaev/blog_engine/Main.java
+++ src/main/java/ru/isakaev/blog_engine/Main.java
@@ -1,17 +1,13 @@
 package ru.isakaev.blog_engine;
 
-import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class Main {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args){
 		SpringApplication.run(Main.class, args);
-		Console.main(args);
 	}
 
 }
